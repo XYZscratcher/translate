a340bd709aea6df6da85b16cdf54dbf85e4f28f4
XYZscratcher <xyzscer@outlook.com>
2022-08-09 22:13:22 +0800

    Update cs4.py

--- cs4.py
+++ cs4.py
@@ -1,12 +1,16 @@
 # 导入 requests 包
-import re
 import requests
 from bs4 import BeautifulSoup
-text=input('翻译哪个单词？\n')
-# 发送请求
-x = requests.get('https://dict.youdao.com/result?word={}&lang=en'.format(text))
-x.encoding="utf-8"
-soup=BeautifulSoup(x.text,'html.parser')
-a=soup.find(class_='trans')
-a="单词 "+text+" 的意思是：\n"+a.text
-print(a)
+while True:
+    text=input('翻译哪个单词？\n')
+    # 发送请求
+    try:
+        x = requests.get('https://dict.youdao.com/result?word={}&lang=en'.format(text))
+        x.encoding="utf-8"
+        soup=BeautifulSoup(x.text,'html.parser')
+        a=soup.find(class_='trans')
+        a="单词 "+text+" 的意思是：\n"+a.text
+        print(a)
+        print("="*30)
+    except AttributeError:
+        raise Exception('你输入的不是一个单词哦！')
